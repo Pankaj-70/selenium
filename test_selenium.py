1988d0dd96bb23f7f3c7ff72a0380de9f1530871
Pankaj-70 <grgnld3970@gmail.com>
2026-03-22 11:48:25 +0530

    google youtube automated search

--- test_selenium.py
+++ test_selenium.py
@@ -1,22 +1,50 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
+sites = ['https://www.youtube.com/','https://net52.cc/home', 'https://google.com', 'https://github.com']
+driver_path = '/home/pankaj/Downloads/chrome-linux64/chrome'
+brave_path = '/bin/brave-browser'
 #configuring browser
 options = Options()
-options.binary_location = "/usr/bin/google-chrome"
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+options.binary_location = brave_path
 
 #start browser
+# service = Service(driver_path)
 driver = webdriver.Chrome(options=options)
 
 #open website
-driver.get('https://google.com')
 
-#extract data
-print('Title', driver.title)
-print('URL:', driver.current_url)
+'''
+for site in sites:
+    driver.get(site)
+
+    #extract data
+    print('Title', driver.title)
+    print('URL:', driver.current_url)
+
+'''
+
+#search by input in google
+# driver.get(sites[0])
+# search = driver.find_element(By.NAME, "q")
+# search.send_keys('rashtriya military school, chail')
+# search.submit()
+
+# #click on captcha button
+# button = driver.find_element(By.XPATH, "//div[@class = 'recaptcha-checkbox-border']")
+
+
+#search by input in youtube
+# driver.get('https://youtube.com')
+# search = driver.find_element(By.NAME, "search_query")
+# search.send_keys('arijit singh latest songs')
+# search.submit()
+
+
+
+
 
 #close browser
 driver.quit()
